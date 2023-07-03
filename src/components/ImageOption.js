46fd2ba2fe1ade918b7837f2b0d184439f4da857
mickcooke <128602534+mickcooke@users.noreply.github.com>
2023-07-03 14:24:45 +0100

    fixed null problem

--- src/components/ImageOption.js
+++ src/components/ImageOption.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 const ImageOption = ({image, value}) => {
+
+if(!image) {
+    return null
+}
+
     return (
         <>
 <option value={value}>
     <div>
-{/* <img src={image.links[0].href}></img>
- */}
+{/* <img src={image.links[0].href}></img> */}
+
  {image.data[0].nasa_id}
     </div>
     </option>
